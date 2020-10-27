7e46239dbc35e31674a092c96bbbd479a19fa3c9
dapper-firmware-5532 <eyesforwardfilms@gmail.com>
2020-10-27 04:17:20 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,42 @@
+function add() {
+}
+function subtract()
+{}
+function multiply()
+{}
+function divide()
+{}
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b ;
+}
+function increment(n){
+  return ++n;
+}
+function decrement(n){
+  return --n;
+}
+
+
+function parsInt(n){
+  return parsInt('n',n)
+}
+function parseInt(){
+return parseInt('n', 10)
+}
+function parseInt(){
+  return parseInt("nonsense",10)
+}
+
+  
+  
